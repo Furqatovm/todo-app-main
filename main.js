d3c6542a2d0609c20fb05a0f8a7255edbb85474e
Furqatovm <muhammadyusuffurqatov91@gmail.com>
2025-03-09 13:11:58 +0500

    prototpye

--- main.js
+++ main.js
@@ -40,6 +40,42 @@ checkbox.addEventListener("click", function(){
     }
 })
 
+let li =document.getElementById("li")
+
+let del =document.getElementById("closeee");
+del.addEventListener("click", function(){
+    li.style.display ="none";
+})
+
+let todo =document.getElementById("todo-input")
+
+
+let ul =document.querySelector("ul");
+
+let button =document.createElement("button");
+let svg =document.createElement("svg");
+
+let matnn =document.createElement("p");
+
+let button2 =document.createElement("button");
+
+let svg2 =document.createElement("svg");
+button.appendChild(svg);
+button2.appendChild(svg2);
+matnn.innerHTML(todo)
+
+todo.addEventListener("keypress", function(event){
+    if(event.key ==="Enter"){
+        button.appendChild(svg);
+        li.className("first-task");
+        button.className("checkbox-tugma");
+        button2.appendChild(svg2);
+        li.appendChild(button, button2, matnn);
+
+    }
+})
+
+
 // checkbox.addEventListener("click", function(){
 //     matn.style.textDecoration ="line-through"
 // })
